31b8ccc77138261f2f032c80d7f6b72b43ae4589
AdrianRDG <adrian.rojas@tergar.org>
2025-09-02 15:57:00 -0600

    Feat: add cors and express-validator libs and implement it on index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 require('dotenv').config();
 const { dbConnection } = require('./database/config');
+const cors = require('cors');
 
 const app = express();
 
+app.use(express.json());
+
 dbConnection();
+
+app.use(cors());
+
+//Routes
+app.use('/api/currency', require('./routes/currency.route'));
 
 app.listen(process.env.APP_PORT, () => console.log('Running in port:' + process.env.APP_PORT));
